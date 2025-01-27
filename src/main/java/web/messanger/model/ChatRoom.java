7a9ca4f93d1389a47027d35d861944baef0d9930
kyungmin123 <kimkm3972@naver.com>
2025-01-27 23:04:18 +0900

    create entities

--- src/main/java/web/messanger/model/ChatRoom.java
+++ src/main/java/web/messanger/model/ChatRoom.java
@@ -1,22 +1,26 @@
 package web.messanger.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class ChatRoom {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String name;
-//
-//    private String type; // private or group
-//
-//    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-//    private List<ChatRoomUser> users = new ArrayList<>();
-//
-//    @CreationTimestamp
-//    private LocalDateTime createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    // Getters and Setters
+    private String name;
+
+    private String type; // private or group
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ChatRoomUser> users = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
---
+++ src/main/java/web/messanger/model/ChatRoomUser.java
@@ -0,0 +1,20 @@
+package web.messanger.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ChatRoomUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Getters and Setters
+}
